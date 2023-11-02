9488ec6c985426422b0127b5e51baf518bada81e
Bruce <brucexiong2000@gmail.com>
2023-11-01 23:15:39 -0700

    Changed the middle square into a sphere

--- xiong.h
+++ xiong.h
@@ -7,7 +7,15 @@ void xiong(bool enabled) {
     //float heights[10] = { 0 };
     //Vector3 positions[10] = { 0 };
     
-    DrawCube((Vector3){ 0.0f, 0.0f, 32.0f }, 5.0f, 1.0f, 5.0f, LIGHTGRAY); //box
+    //DrawCylinderWires(Vector3 position, float radiusTop, float radiusBottom, float height, int slices, Color color); Draw a cylinder/cone wires
+    //DrawCylinder(Vector3 position, float radiusTop, float radiusBottom, float height, int slices, Color color); cylinder function
+    //DrawSphereWires(Vector3 centerPos, float radius, int rings, int slices, Color color); sphere wires function
+    
+    //DrawCube((Vector3){ 0.0f, 0.0f, 32.0f }, 5.0f, 1.0f, 5.0f, LIGHTGRAY); //box
+    DrawCylinder((Vector3){ 0.0f, 0.0f, 32.0f } , 3.0f, 3.0f, 1.0f, 20, LIGHTGRAY);
+    DrawCylinder((Vector3){ 0.0f, 0.0f, 32.0f } , 2.8f, 2.8f, 1.2f, 20, SKYBLUE);
+    DrawCylinderWires((Vector3){0.0f, 0.0f, 32.0f} , 2.8f, 2.8f, 1.3f, 20, BLUE); // Draw a cylinder wires   
+    DrawSphereWires(Vector3{ 0.0f, 3.5f, 32.0f }, 2.0f, 10, 10, GOLD); 
     
     /*for (int i = 0; i < 10; i++) {
         DrawCube(positions[i], 2.0f, heights[i], 2.0f, LIGHTGRAY);
