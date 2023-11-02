b1cf9f6efd6facfecdc2f9b908541a3599549f17
Robert Voss <roboalex123@gmail.com>
2023-11-01 23:48:38 -0700

    Added broken walls and a spire

--- main.cc
+++ main.cc
@@ -4,7 +4,8 @@
     William Kerney
     Sean Raymond
     Meagan Eggert
-    Robert Voss
+    Ro
+     bert Voss
     Bruce Xiong
 */
 
@@ -23,50 +24,48 @@ int main(void)
     //--------------------------------------------------------------------------------------
     const int screenWidth = 800;
     const int screenHeight = 450;
-    
-
 
     InitWindow(screenWidth, screenHeight, "raylib [core] example - 3d camera first person");
-    
+
     // Initialize audio device, so you can play sounds - Meg
     InitAudioDevice();
     // This is a cool laser sound
     Sound laser = LoadSound("laser.wav");
     Music bgMusic = LoadMusicStream("Space Music2.mp3");
     PlayMusicStream(bgMusic); // Play Background Music
-    
+
     // Define the camera to look into our 3d world (position, target, up vector)
-    Camera camera = { 0 };
-    camera.position = (Vector3){ 0.0f, 2.0f, 4.0f };    // Camera position
-    camera.target = (Vector3){ 0.0f, 2.0f, 0.0f };      // Camera looking at point
-    camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };          // Camera up vector (rotation towards target)
-    camera.fovy = 60.0f;                                // Camera field-of-view Y
-    camera.projection = CAMERA_PERSPECTIVE;             // Camera projection type
+    Camera camera = {0};
+    camera.position = (Vector3){0.0f, 2.0f, 4.0f}; // Camera position
+    camera.target = (Vector3){0.0f, 2.0f, 0.0f};   // Camera looking at point
+    camera.up = (Vector3){0.0f, 1.0f, 0.0f};       // Camera up vector (rotation towards target)
+    camera.fovy = 60.0f;                           // Camera field-of-view Y
+    camera.projection = CAMERA_PERSPECTIVE;        // Camera projection type
 
     int cameraMode = CAMERA_FIRST_PERSON;
 
     // Generates some random columns
-    float heights[MAX_COLUMNS] = { 0 };
-    Vector3 positions[MAX_COLUMNS] = { 0 };
-    Color colors[MAX_COLUMNS] = { 0 };
+    float heights[MAX_COLUMNS] = {0};
+    Vector3 positions[MAX_COLUMNS] = {0};
+    Color colors[MAX_COLUMNS] = {0};
 
     for (int i = 0; i < MAX_COLUMNS; i++)
     {
         heights[i] = (float)GetRandomValue(1, 12);
-        positions[i] = (Vector3){ (float)GetRandomValue(-15, 15), heights[i]/2.0f, (float)GetRandomValue(-15, 15) };
-        colors[i] = (Color){ GetRandomValue(20, 255), GetRandomValue(10, 55), 30, 255 };
+        positions[i] = (Vector3){(float)GetRandomValue(-15, 15), heights[i] / 2.0f, (float)GetRandomValue(-15, 15)};
+        colors[i] = (Color){GetRandomValue(20, 255), GetRandomValue(10, 55), 30, 255};
     }
 
-    DisableCursor();                    // Limit cursor to relative movement inside the window
+    DisableCursor(); // Limit cursor to relative movement inside the window
 
-    SetTargetFPS(60);                   // Set our game to run at 60 frames-per-second
+    SetTargetFPS(60); // Set our game to run at 60 frames-per-second
     //--------------------------------------------------------------------------------------
 
     // Main game loop
-    while (!WindowShouldClose())        // Detect window close button or ESC key
+    while (!WindowShouldClose()) // Detect window close button or ESC key
     {
         UpdateMusicStream(bgMusic);
-        
+
         // Press 'k' to play a laser sound
         if (IsKeyPressed(KEY_K))
         {
@@ -132,113 +131,113 @@ int main(void)
         // Update camera computes movement internally depending on the camera mode
         // Some default standard keyboard/mouse inputs are hardcoded to simplify use
         // For advance camera controls, it's reecommended to compute camera movement manually
-        UpdateCamera(&camera, cameraMode);                  // Update camera
+        UpdateCamera(&camera, cameraMode); // Update camera
 
-/*
-        // Camera PRO usage example (EXPERIMENTAL)
-        // This new camera function allows custom movement/rotation values to be directly provided
-        // as input parameters, with this approach, rcamera module is internally independent of raylib inputs
-        UpdateCameraPro(&camera,
-            (Vector3){
-                (IsKeyDown(KEY_W) || IsKeyDown(KEY_UP))*0.1f -      // Move forward-backward
-                (IsKeyDown(KEY_S) || IsKeyDown(KEY_DOWN))*0.1f,    
-                (IsKeyDown(KEY_D) || IsKeyDown(KEY_RIGHT))*0.1f -   // Move right-left
-                (IsKeyDown(KEY_A) || IsKeyDown(KEY_LEFT))*0.1f,
-                0.0f                                                // Move up-down
-            },
-            (Vector3){
-                GetMouseDelta().x*0.05f,                            // Rotation: yaw
-                GetMouseDelta().y*0.05f,                            // Rotation: pitch
-                0.0f                                                // Rotation: roll
-            },
-            GetMouseWheelMove()*2.0f);                              // Move to target (zoom)
-*/
+        /*
+                // Camera PRO usage example (EXPERIMENTAL)
+                // This new camera function allows custom movement/rotation values to be directly provided
+                // as input parameters, with this approach, rcamera module is internally independent of raylib inputs
+                UpdateCameraPro(&camera,
+                    (Vector3){
+                        (IsKeyDown(KEY_W) || IsKeyDown(KEY_UP))*0.1f -      // Move forward-backward
+                        (IsKeyDown(KEY_S) || IsKeyDown(KEY_DOWN))*0.1f,
+                        (IsKeyDown(KEY_D) || IsKeyDown(KEY_RIGHT))*0.1f -   // Move right-left
+                        (IsKeyDown(KEY_A) || IsKeyDown(KEY_LEFT))*0.1f,
+                        0.0f                                                // Move up-down
+                    },
+                    (Vector3){
+                        GetMouseDelta().x*0.05f,                            // Rotation: yaw
+                        GetMouseDelta().y*0.05f,                            // Rotation: pitch
+                        0.0f                                                // Rotation: roll
+                    },
+                    GetMouseWheelMove()*2.0f);                              // Move to target (zoom)
+        */
         //----------------------------------------------------------------------------------
 
         // Draw
         //----------------------------------------------------------------------------------
         bool xiong_enabled = true;
         bool eggert_enabled = true;
-        bool voss_enabled = false;
+        bool voss_enabled = true; // voss
         const bool kerney_enabled = true;
         bool raymond_enabled = false;
         BeginDrawing();
 
-            ClearBackground(RAYWHITE);
+        ClearBackground(RAYWHITE);
 
-            //TODO:
-            //Add in a skybox
-            //Add in models for the spaceship
-            //Add in monsters
-            //Raymond can add jumping and sprinting and collision
-            //Add in lighting/electricity puzzle
-            //Weapons
-            BeginMode3D(camera);
+        // TODO:
+        // Add in a skybox
+        // Add in models for the spaceship
+        // Add in monsters
+        // Raymond can add jumping and sprinting and collision
+        // Add in lighting/electricity puzzle
+        // Weapons
+        BeginMode3D(camera);
 
-                xiong(xiong_enabled);
-                eggert(eggert_enabled);
-                voss(voss_enabled);
-                kerney(kerney_enabled);
-                raymond(raymond_enabled);
+        xiong(xiong_enabled);
+        eggert(eggert_enabled);
+        voss(voss_enabled);
+        kerney(kerney_enabled);
+        raymond(raymond_enabled);
 
-                DrawPlane((Vector3){ 0.0f, 0.0f, 0.0f }, (Vector2){ 32.0f, 32.0f }, GOLD); // Draw ground
-                /*
-                DrawCube((Vector3){ -16.0f, 2.5f, 0.0f }, 1.0f, 5.0f, 32.0f, BLUE);     // Draw a blue wall
-                DrawCube((Vector3){ 16.0f, 2.5f, 0.0f }, 1.0f, 5.0f, 32.0f, LIME);      // Draw a green wall
-           `     DrawCube((Vector3){ 0.0f, 2.5f, 16.0f }, 32.0f, 5.0f, 1.0f, GOLD);      // Draw a yellow wall
-                */
+        DrawPlane((Vector3){0.0f, 0.0f, 0.0f}, (Vector2){32.0f, 32.0f}, GOLD); // Draw ground
+        /*
+        DrawCube((Vector3){ -16.0f, 2.5f, 0.0f }, 1.0f, 5.0f, 32.0f, BLUE);     // Draw a blue wall
+        DrawCube((Vector3){ 16.0f, 2.5f, 0.0f }, 1.0f, 5.0f, 32.0f, LIME);      // Draw a green wall
+   `     DrawCube((Vector3){ 0.0f, 2.5f, 16.0f }, 32.0f, 5.0f, 1.0f, GOLD);      // Draw a yellow wall
+        */
 
-                // Draw some cubes around
-                for (int i = 0; i < MAX_COLUMNS; i++)
-                {
-                    DrawCube(positions[i], 2.0f, heights[i], 2.0f, colors[i]);
-                    DrawCubeWires(positions[i], 2.0f, heights[i], 2.0f, MAROON);
-                }
+        // Draw some cubes around
+        for (int i = 0; i < MAX_COLUMNS; i++)
+        {
+            DrawCube(positions[i], 2.0f, heights[i], 2.0f, colors[i]);
+            DrawCubeWires(positions[i], 2.0f, heights[i], 2.0f, MAROON);
+        }
 
-                /*
-                // Draw player cube
-                if (cameraMode == CAMERA_THIRD_PERSON)
-                {
-                    DrawCube(camera.target, 0.5f, 0.5f, 0.5f, PURPLE);
-                    DrawCubeWires(camera.target, 0.5f, 0.5f, 0.5f, DARKPURPLE);
-                }
-                */
+        /*
+        // Draw player cube
+        if (cameraMode == CAMERA_THIRD_PERSON)
+        {
+            DrawCube(camera.target, 0.5f, 0.5f, 0.5f, PURPLE);
+            DrawCubeWires(camera.target, 0.5f, 0.5f, 0.5f, DARKPURPLE);
+        }
+        */
 
-            EndMode3D();
+        EndMode3D();
 
-            // Draw info boxes
-            DrawRectangle(5, 5, 330, 100, Fade(SKYBLUE, 0.5f));
-            DrawRectangleLines(5, 5, 330, 100, BLUE);
+        // Draw info boxes
+        DrawRectangle(5, 5, 330, 100, Fade(SKYBLUE, 0.5f));
+        DrawRectangleLines(5, 5, 330, 100, BLUE);
 
-            DrawText("Camera controls:", 15, 15, 10, BLACK);
-            DrawText("- Move keys: W, A, S, D, Space, Left-Ctrl", 15, 30, 10, BLACK);
-            DrawText("- Look around: arrow keys or mouse", 15, 45, 10, BLACK);
-            DrawText("- Camera mode keys: 1, 2, 3, 4", 15, 60, 10, BLACK);
-            DrawText("- Zoom keys: num-plus, num-minus or mouse scroll", 15, 75, 10, BLACK);
-            DrawText("- Camera projection key: P", 15, 90, 10, BLACK);
+        DrawText("Camera controls:", 15, 15, 10, BLACK);
+        DrawText("- Move keys: W, A, S, D, Space, Left-Ctrl", 15, 30, 10, BLACK);
+        DrawText("- Look around: arrow keys or mouse", 15, 45, 10, BLACK);
+        DrawText("- Camera mode keys: 1, 2, 3, 4", 15, 60, 10, BLACK);
+        DrawText("- Zoom keys: num-plus, num-minus or mouse scroll", 15, 75, 10, BLACK);
+        DrawText("- Camera projection key: P", 15, 90, 10, BLACK);
 
-/*
-            DrawRectangle(600, 5, 195, 100, Fade(SKYBLUE, 0.5f));
-            DrawRectangleLines(600, 5, 195, 100, BLUE);
+        /*
+                    DrawRectangle(600, 5, 195, 100, Fade(SKYBLUE, 0.5f));
+                    DrawRectangleLines(600, 5, 195, 100, BLUE);
 
-            DrawText("Camera status:", 610, 15, 10, BLACK);
-            DrawText(TextFormat("- Mode: %s", (cameraMode == CAMERA_FREE) ? "FREE" :
-                                              (cameraMode == CAMERA_FIRST_PERSON) ? "FIRST_PERSON" :
-                                              (cameraMode == CAMERA_THIRD_PERSON) ? "THIRD_PERSON" :
-                                              (cameraMode == CAMERA_ORBITAL) ? "ORBITAL" : "CUSTOM"), 610, 30, 10, BLACK);
-            DrawText(TextFormat("- Projection: %s", (camera.projection == CAMERA_PERSPECTIVE) ? "PERSPECTIVE" :
-                                                    (camera.projection == CAMERA_ORTHOGRAPHIC) ? "ORTHOGRAPHIC" : "CUSTOM"), 610, 45, 10, BLACK);
-            DrawText(TextFormat("- Position: (%06.3f, %06.3f, %06.3f)", camera.position.x, camera.position.y, camera.position.z), 610, 60, 10, BLACK);
-            DrawText(TextFormat("- Target: (%06.3f, %06.3f, %06.3f)", camera.target.x, camera.target.y, camera.target.z), 610, 75, 10, BLACK);
-            DrawText(TextFormat("- Up: (%06.3f, %06.3f, %06.3f)", camera.up.x, camera.up.y, camera.up.z), 610, 90, 10, BLACK);
-*/
+                    DrawText("Camera status:", 610, 15, 10, BLACK);
+                    DrawText(TextFormat("- Mode: %s", (cameraMode == CAMERA_FREE) ? "FREE" :
+                                                      (cameraMode == CAMERA_FIRST_PERSON) ? "FIRST_PERSON" :
+                                                      (cameraMode == CAMERA_THIRD_PERSON) ? "THIRD_PERSON" :
+                                                      (cameraMode == CAMERA_ORBITAL) ? "ORBITAL" : "CUSTOM"), 610, 30, 10, BLACK);
+                    DrawText(TextFormat("- Projection: %s", (camera.projection == CAMERA_PERSPECTIVE) ? "PERSPECTIVE" :
+                                                            (camera.projection == CAMERA_ORTHOGRAPHIC) ? "ORTHOGRAPHIC" : "CUSTOM"), 610, 45, 10, BLACK);
+                    DrawText(TextFormat("- Position: (%06.3f, %06.3f, %06.3f)", camera.position.x, camera.position.y, camera.position.z), 610, 60, 10, BLACK);
+                    DrawText(TextFormat("- Target: (%06.3f, %06.3f, %06.3f)", camera.target.x, camera.target.y, camera.target.z), 610, 75, 10, BLACK);
+                    DrawText(TextFormat("- Up: (%06.3f, %06.3f, %06.3f)", camera.up.x, camera.up.y, camera.up.z), 610, 90, 10, BLACK);
+        */
         EndDrawing();
         //----------------------------------------------------------------------------------
     }
 
     // De-Initialization
     //--------------------------------------------------------------------------------------
-    CloseWindow();        // Close window and OpenGL context
+    CloseWindow(); // Close window and OpenGL context
     CloseAudioDevice();
     UnloadSound(laser);
     UnloadMusicStream(bgMusic);
@@ -246,4 +245,3 @@ int main(void)
 
     return 0;
 }
-
--- voss.h
+++ voss.h
@@ -1,3 +1,33 @@
-void voss(bool enabled) {
+#pragma once
+#include <stdbool.h>
+#include "raylib.h"
 
+void voss(bool enabled)
+{
+    float size = 32.0;
+
+    // Define Origin
+    Vector3 origin = (Vector3){-size, 0.0f, 0.0f};
+
+    // Draw a cylinder shape in the middle of the floor (origin + (half size, 0, half size)) with a radius of 1.0 and a height of 2.0
+    DrawCylinder(origin, 1.0f, 2.0f, 10.0f, 7, RED);
+
+    // Draw Ground
+    DrawPlane(origin, (Vector2){size, size}, BLUE);
+
+    // Draw Walls on the sides
+    float wallHeight = 10.0f;
+    float wallThickness = 2.0f;
+
+    // North Wall
+    Vector3 northWall_orig = (Vector3){-size, wallHeight / 2.0, size - size / 2.0};
+    // South Wall
+    Vector3 southWall_orig = (Vector3){-size, wallHeight / 2.0, -(size + size / 2.0)};
+    // East Wall
+    Vector3 eastWall_orig = (Vector3){-size + size / 2.0, wallHeight / 2.0, -size};
+
+    // Draw Walls
+    DrawCube(northWall_orig, size, wallHeight, wallThickness, GRAY);
+    DrawCube(southWall_orig, size, wallHeight, wallThickness, GRAY);
+    DrawCube(eastWall_orig, wallThickness, wallHeight, size, GRAY);
 }
