b283ec32284382d2aa535c5910cac5fb4128047c
Meagan Eggert <meagan.eggert@gmail.com>
2023-11-07 13:24:19 -0800

    Fixing narrowing warnings

--- eggert.h
+++ eggert.h
@@ -8,7 +8,7 @@ void eggert(bool enabled) {
 
         
         
-        double size = 32.0;
+        float size = 32.0;
         
         // Define Origin
         Vector3 origin = (Vector3){ 0.0, 0.0, -size };
@@ -21,15 +21,15 @@ void eggert(bool enabled) {
         DrawPlane(origin, (Vector2){ size, size }, BLACK);
         
         // All Walls
-        double wallHeight = 10.0;
-        double wallThickness = 2.0;
+        float wallHeight = 10.0;
+        float wallThickness = 2.0;
         
         // South Wall
-        Vector3 southWall_orig = (Vector3){0.0, wallHeight / 2.0, -(size + size / 2.0)};
+        Vector3 southWall_orig = (Vector3){0.0f, wallHeight / 2.0f, -(size + size / 2.0f)};
         // West Wall
-        Vector3 eastWall_orig = (Vector3){-size + size / 2.0, wallHeight / 2.0, -size};
+        Vector3 eastWall_orig = (Vector3){-size + size / 2.0f, wallHeight / 2.0f, -size};
         // East Wall
-        Vector3 westWall_orig = (Vector3){size - size / 2.0, wallHeight / 2.0, -size};
+        Vector3 westWall_orig = (Vector3){size - size / 2.0f, wallHeight / 2.0f, -size};
         
         // Draw Walls
         DrawCube(southWall_orig, size, wallHeight, wallThickness, DARKGRAY);
@@ -37,8 +37,8 @@ void eggert(bool enabled) {
         DrawCube(eastWall_orig, wallThickness, wallHeight, size, GRAY);
         
         // Random Cubes
-        float maxPos_Z = 10.0f;
-        float minPos_Z = 0.0f;
+        //float maxPos_Z = 10.0f;
+        //float minPos_Z = 0.0f;
         
 
     }     
