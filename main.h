19db00ef55e8911bf58fa1addd6a1a17157f1876
Meagan Eggert <meagan.eggert@gmail.com>
2023-11-07 12:17:52 -0800

    Adding in Camera Header Files

--- main.h
+++ main.h
@@ -6,6 +6,32 @@
 #include "raylib.h"
 #include "rcameracopy.h"
 #include "shapes.h"
+#include "rlTPCamera.h"
+using namespace std;
+
+#include "xiong.h"
+#include "voss.h"
+#include "raymond.h"
+#include "kerney.h"
+#include "eggert.h"
+
+/*
+void xiong(bool enabled);
+void voss(bool enabled);
+void eggert(bool enabled);
+void raymond(bool enabled);
+void kerney(bool enabled);
+*/
+
+#endif#ifndef MAIN_H
+#define MAIN_H
+//Common header file
+#include <iostream>
+#include <vector>
+#include "raylib.h"
+#include "rcameracopy.h"
+#include "shapes.h"
+#include "rlTPCamera.h"
 using namespace std;
 
 #include "xiong.h"
